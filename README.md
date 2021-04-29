a6d1ef20b2fc6c17f355777293e0225006bf961a
zhangyu <zy2071@126.com>
2021-04-29 14:25:25 +0800

    feat：✨ 代码逻辑优化

--- README.md
+++ README.md
@@ -1,16 +1,16 @@
 # Crawler-for-Github-Trending  
-50 lines, minimalist node crawler for [Trending](https://github.com/trending?since=daily).  
+50 lines, minimalist node crawler for [GitHub Trending](https://github.com/trending?since=daily).  
 一个50行的 node 爬虫，一个简单的 [axios](https://github.com/axios/axios), [express](https://github.com/expressjs/express), [cheerio](https://github.com/cheeriojs/cheerio) 体验项目。  
 
 # Usage  
 一篇简单的介绍 https://juejin.im/post/5cbab247e51d45789024d7cb   
-一个简单的应用 http://zy2071.com/Fun/todayGithub.html  
+一个简单的应用 https://zy2071.com/project-center/#/githubTrending  
 
 首先保证电脑已存在 node10.0+ 环境，然后  
 
 1.拉取本项目  
 ```
-git clone https://github.com/ZY2071/Crawler-for-Github-Trending.git
+git clone https://github.com/poozhu/Crawler-for-Github-Trending.git
 cd Crawler-for-Github-Trending
 npm i
 node index.js
@@ -27,12 +27,12 @@ node index.js
 ```
 Listening on port 3000!
 ```
-此时打开浏览器，进入本地服务 http://localhost:3000/daily 
+此时打开浏览器，访问 http://localhost:3000/
 ```
-http://localhost:3000/time-language // time 表示周期，language 代表语言  例如：
+http://localhost:3000/list/:time/:language // time 表示周期，language 代表语言  例如：
 
-http://localhost:3000/daily  // 代表今日 可选参数：weekly,monthly
-http://localhost:3000/daily-JavaScript  // 代表今日的 JavaScript 分类 可选参数：任意语言
+http://localhost:3000/list/daily  // 代表今日 可选参数：weekly,monthly
+http://localhost:3000/list/daily/JavaScript  // 代表今日的 JavaScript 分类 可选参数：任意语言
 ```
 
 稍微等待即可看到爬取完毕的返回数据：
@@ -61,6 +61,6 @@ http://localhost:3000/daily-JavaScript  // 代表今日的 JavaScript 分类 可
 ```
 
 # More
-本项目每次访问都会实时爬取数据，所以数据返回速度会非常慢，期望作为接口数据请定时爬取到数据库。
+本项目每次访问都会实时爬取数据，所以数据返回速度会比较慢，期望作为接口数据建议定时爬取到数据库。
 
 但了解项目代码可以带来以上各个 node 模块和爬虫最基础的用法和概念，希望可以帮到大家。
--- index.js
+++ index.js
@@ -3,69 +3,65 @@ const axios = require("axios");
 const express = require("express");
 const app = express();
 
-function getData(time, language) {
-    let url = "https://github.com/trending" + (!!language ? "/" + language : "") + "?since=" + time; // 拼接请求的页面链接
-    return axios
-        .get(url)
-        .then(function (response) {
-            let html_string = response.data.toString(); // 获取网页内容
-            const $ = cheerio.load(html_string); // 传入页面内容
-            let list_array = [];
-            $(".Box .Box-row").each(function () {
-                // 像jQuery一样获取对应节点值
-                let obj = {};
-                obj.title = $(this).find("h1").text().trimStart().trimEnd(); // 获取标题
-                obj.links = "https://github.com/" + obj.title.replace(/\s/g, ""); // 拼接链接
-                obj.description = $(this).find("p").text().trimStart().trimEnd(); // 获取获取描述
-                obj.language = $(this).find(">.f6 .repo-language-color").siblings().text().trimStart().trimEnd(); // 获取语言
-                obj.stars = $(this).find(">.f6 a").eq(0).text().trimStart().trimEnd(); // 获取 start 数
-                obj.forks = $(this).find(">.f6 a").eq(1).text().trimStart().trimEnd(); // 获取分支数
-                obj.info = $(this).find(">.f6 .float-sm-right").text().trimStart().trimEnd(); // 获取对应时期 star 信息
-                obj.avatar = $(this).find(">.f6 img").eq(0).attr("src"); // 获取首位作者头像
-                list_array.push(obj);
+async function getData(time, language) {
+    const url = "https://github.com/trending" + (!!language ? "/" + language : "") + "?since=" + time; // 拼接请求的页面链接
+    // console.log(url); // 检查请求的 url 地址
+    const response = await axios.get(url).catch(function (error) {
+        return error;
+    });
 
-                // 检测各项数据是否正确
-                // console.log(obj);
-            });
+    // 不存在 data 字段时，直接返回 （请求出现了错误）
+    if (!response.data) {
+        return response;
+    }
 
-            // 回归按新增 star 数量排名
-            list_array = list_array.sort((x, y) => {
-                return parseInt(y.info.replace(/,/, "")) - parseInt(x.info.replace(/,/, ""));
-            });
+    const $ = cheerio.load(response.data); // 传入页面内容
+    let items_array = [];
+    $(".Box .Box-row").each(function () {
+        // 像jQuery一样获取对应节点值
+        let obj = {};
+        obj.title = $(this).find("h1").text().replace(/\s/g, ""); // 获取标题
+        obj.links = "https://github.com/" + obj.title; // 拼接链接
+        obj.description = $(this).find("p").text().trim(); // 获取获取描述
+        obj.language = $(this).find(">.f6 .repo-language-color").siblings().text(); // 获取语言
+        obj.stars = $(this).find(">.f6 a").eq(0).text().trim(); // 获取 start 数
+        obj.forks = $(this).find(">.f6 a").eq(1).text().trim(); // 获取分支数
+        obj.info = $(this).find(">.f6 .float-sm-right").text().trim(); // 获取对应时期 star 信息
+        obj.avatar = $(this).find(">.f6 img").eq(0).attr("src"); // 获取首位作者头像
+        items_array.push(obj);
 
-            return Promise.resolve(list_array);
-        })
-        .catch(function (error) {
-            console.log(error);
-        });
+        // 检测各项数据是否正确
+        // console.log(obj);
+    });
+
+    // 回归按新增 star 数量排名
+    items_array.sort((item1, item2) => {
+        return parseInt(item2.info.replace(/,/, "")) - parseInt(item1.info.replace(/,/, ""));
+    });
+
+    return items_array;
 }
 
-app.get("/", (req, res) => {
-    let promise = getData("daily"); // 发起抓取
-    promise.then((response) => {
-        res.json(response); // 数据返回
-    });
-});
-
-app.get("/:time-:language", (req, res) => {
+app.get("/list/:time/:language", async (req, res) => {
     const {
         time, // 获取排序时间
         language, // 获取对应语言
     } = req.params;
-    let promise = getData(time, language); // 发起抓取
-    promise.then((response) => {
-        res.json(response); // 数据返回
-    });
+    let list = await getData(time, language); // 发起抓取
+    res.json(list); // 数据返回
 });
 
-app.get("/:time", (req, res) => {
+app.get("/list/:time", async (req, res) => {
     const {
         time, // 获取排序时间
     } = req.params;
-    let promise = getData(time); // 发起抓取
-    promise.then((response) => {
-        res.json(response); // 数据返回
-    });
+    let list = await getData(time); // 发起抓取
+    res.json(list); // 数据返回
+});
+
+app.get("/", async (req, res) => {
+    let list = await getData("daily"); // 发起抓取
+    res.json(list); // 数据返回
 });
 
 app.listen(3000, () => console.log("Listening on port 3000!")); // 监听3000端口
